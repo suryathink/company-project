2f84fe91c3df247713780b76f2b1647c90967e96
suryathink <suryaprakashsingh110@gmail.com>
2023-08-21 12:45:42 +0530

    message

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-// git config --global user.email "suryaprakashsingh110@gmail.com"
-// git config --global user.name "suryathink"
+
 function App() {
   return (
     <div className="App">
