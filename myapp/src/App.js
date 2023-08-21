dc19e00790a36b79bb385df837287694e4517f0a
suryathink <suryaprakashsingh110@gmail.com>
2023-08-21 12:33:42 +0530

    first commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-
+// git config --global user.email "suryaprakashsingh110@gmail.com"
+// git config --global user.name "suryathink"
 function App() {
   return (
     <div className="App">
